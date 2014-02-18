9dcfa580509f82abc326fe3a62c4a293b71e3039
PN <pniep@pniep-opcja>
2014-02-18 19:24:37 +0100

    support for multiple filters per field (prevents overwriting of parameters)

--- FilterMapper/ComparisionFilterMapper.php
+++ FilterMapper/ComparisionFilterMapper.php
@@ -2,6 +2,12 @@
 namespace EPS\JqGridBundle\FilterMapper;
 class ComparisionFilterMapper extends AbstractFilterMapper
 {
+
+    /**
+     * @var integer
+     */
+    protected static $cnt = 0;
+    
     /**
      * @param array $rule
      * @param string $groupOperator
@@ -14,38 +20,40 @@ class ComparisionFilterMapper extends AbstractFilterMapper
         $queryBuilder = $this->grid->getQueryBuilder();
         $expression = $this->grid->getQueryBuilder()->expr();
 
+        $tmpid = self::$cnt++;
+        
         switch ($rule['op']) {
             case 'eq':
-                $where = $expression->eq($this->column->getFieldIndex(), ":{$this->column->getFieldName()}");
+                $where = $expression->eq($this->column->getFieldIndex(), ":{$this->column->getFieldName()}_{$tmpid}");
                 break;
 
             case 'ne':
-                $where = $expression->neq($this->column->getFieldIndex(), ":{$this->column->getFieldName()}");
+                $where = $expression->neq($this->column->getFieldIndex(), ":{$this->column->getFieldName()}_{$tmpid}");
                 break;
 
             case 'lt':
-                $where = $expression->lt($this->column->getFieldIndex(), ":{$this->column->getFieldName()}");
+                $where = $expression->lt($this->column->getFieldIndex(), ":{$this->column->getFieldName()}_{$tmpid}");
                 break;
 
             case 'le':
-                $where = $expression->lte($this->column->getFieldIndex(), ":{$this->column->getFieldName()}");
+                $where = $expression->lte($this->column->getFieldIndex(), ":{$this->column->getFieldName()}_{$tmpid}");
                 break;
 
             case 'gt':
-                $where = $expression->gt($this->column->getFieldIndex(), ":{$this->column->getFieldName()}");
+                $where = $expression->gt($this->column->getFieldIndex(), ":{$this->column->getFieldName()}_{$tmpid}");
                 break;
 
             case 'ge':
-                $where = $expression->gte($this->column->getFieldIndex(), ":{$this->column->getFieldName()}");
+                $where = $expression->gte($this->column->getFieldIndex(), ":{$this->column->getFieldName()}_{$tmpid}");
                 break;
 
             case 'bw':
-                $where = 'LOWER(' . $this->column->getFieldIndex() . ") LIKE :{$this->column->getFieldName()}";
+                $where = 'LOWER(' . $this->column->getFieldIndex() . ") LIKE :{$this->column->getFieldName()}_{$tmpid}";
                 $parameter = strtolower($rule['data']) . '%';
                 break;
 
             case 'bn':
-                $where = 'LOWER(' . $this->column->getFieldIndex() . ") NOT LIKE :{$this->column->getFieldName()}";
+                $where = 'LOWER(' . $this->column->getFieldIndex() . ") NOT LIKE :{$this->column->getFieldName()}_{$tmpid}";
                 $parameter = strtolower($rule['data']) . '%';
                 break;
 
@@ -53,7 +61,7 @@ class ComparisionFilterMapper extends AbstractFilterMapper
                 $where = $expression
                         ->orX(
                                 $expression
-                                        ->eq($this->column->getFieldIndex(), ":{$this->column->getFieldName()}"),
+                                        ->eq($this->column->getFieldIndex(), ":{$this->column->getFieldName()}_{$tmpid}"),
                                 $this->column->getFieldIndex() . ' IS NULL');
 
                 $parameter = '';
@@ -63,7 +71,7 @@ class ComparisionFilterMapper extends AbstractFilterMapper
                 $where = $expression
                         ->andX(
                                 $expression
-                                        ->neq($this->column->getFieldIndex(), ":{$this->column->getFieldName()}"),
+                                        ->neq($this->column->getFieldIndex(), ":{$this->column->getFieldName()}_{$tmpid}"),
                                 $this->column->getFieldIndex() . ' IS NOT NULL');
 
                 $parameter = '';
@@ -72,17 +80,17 @@ class ComparisionFilterMapper extends AbstractFilterMapper
             case 'in':
                 if (false !== strpos($rule['data'], ',')) {
 
-                    $where = $expression->in($this->column->getFieldIndex(), ":{$this->column->getFieldName()}");
+                    $where = $expression->in($this->column->getFieldIndex(), ":{$this->column->getFieldName()}_{$tmpid}");
                     $parameter = explode(',', $rule['data']);
 
                 } elseif (false !== strpos($rule['data'], '-')) {
 
-                    $where = $expression->between($this->column->getFieldIndex(), ":start", ":end");
+                    $where = $expression->between($this->column->getFieldIndex(), ":start_{$tmpid}", ":end_{$tmpid}");
 
                     list($start, $end) = explode('-', $rule['data']);
 
-                    $queryBuilder->setParameter('start', $start);
-                    $queryBuilder->setParameter('end', $end);
+                    $queryBuilder->setParameter('start_'.$tmpid, $start);
+                    $queryBuilder->setParameter('end_'.$tmpid, $end);
 
                     unset($parameter);
                 }
@@ -91,37 +99,37 @@ class ComparisionFilterMapper extends AbstractFilterMapper
             case 'ni':
                 if (false !== strpos($rule['data'], ',')) {
 
-                    $where = $expression->notIn($this->column->getFieldIndex(), ":{$this->column->getFieldName()}");
+                    $where = $expression->notIn($this->column->getFieldIndex(), ":{$this->column->getFieldName()}_{$tmpid}");
                     $parameter = explode(',', $rule['data']);
 
                 } elseif (false !== strpos($rule['data'], '-')) {
 
-                    $where = $expression->orX($this->column->getFieldIndex() . "< :start", $this->column->getFieldIndex() . "> :end");
+                    $where = $expression->orX($this->column->getFieldIndex() . "< :start_{$tmpid}", $this->column->getFieldIndex() . "> :end_{$tmpid}");
                     list($start, $end) = explode('-', $rule['data']);
-                    $queryBuilder->setParameter('start', $start);
-                    $queryBuilder->setParameter('end', $end);
+                    $queryBuilder->setParameter('start_'.$tmpid, $start);
+                    $queryBuilder->setParameter('end_'.$tmpid, $end);
                     unset($parameter);
                 }
 
                 break;
 
             case 'ew':
-                $where = 'LOWER(' . $this->column->getFieldIndex() . ") LIKE :{$this->column->getFieldName()}";
+                $where = 'LOWER(' . $this->column->getFieldIndex() . ") LIKE :{$this->column->getFieldName()}_{$tmpid}";
                 $parameter = '%' . strtolower($rule['data']);
                 break;
 
             case 'en':
-                $where = 'LOWER(' . $this->column->getFieldIndex() . ") NOT LIKE :{$this->column->getFieldName()}";
+                $where = 'LOWER(' . $this->column->getFieldIndex() . ") NOT LIKE :{$this->column->getFieldName()}_{$tmpid}";
                 $parameter = '%' . strtolower($rule['data']);
                 break;
 
             case 'nc':
-                $where = 'LOWER(' . $this->column->getFieldIndex() . ") NOT LIKE :{$this->column->getFieldName()}";
+                $where = 'LOWER(' . $this->column->getFieldIndex() . ") NOT LIKE :{$this->column->getFieldName()}_{$tmpid}";
                 $parameter = '%' . strtolower($rule['data']) . '%';
                 break;
 
             default: // Case 'cn' (contains)
-                $where = 'LOWER(' . $this->column->getFieldIndex() . ") LIKE :{$this->column->getFieldName()}";
+                $where = 'LOWER(' . $this->column->getFieldIndex() . ") LIKE :{$this->column->getFieldName()}_{$tmpid}";
                 $parameter = '%' . strtolower($rule['data']) . '%';
         }
 
@@ -132,7 +140,7 @@ class ComparisionFilterMapper extends AbstractFilterMapper
         }
 
         if (isset($parameter)) {
-            $queryBuilder->setParameter($this->column->getFieldName(), $parameter);
+            $queryBuilder->setParameter($this->column->getFieldName().'_'.$tmpid, $parameter);
         }
     }
 }
